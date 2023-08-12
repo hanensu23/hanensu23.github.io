877d34f448d0cca6159fc3995a6faf56d3dd77cb
Hanen <hanensu23@berkley.edu>
2023-08-11 22:16:42 -0700

    dark and light mode now both functional, changed background to turqoise

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-// script.js
 document.addEventListener("DOMContentLoaded", function() {
     const darkModeToggle = document.getElementById("dark-mode-toggle");
     const body = document.body;
+    let isDarkMode = false;
 
     darkModeToggle.addEventListener("click", function() {
-        body.classList.toggle("dark-mode"); // Toggle the dark-mode class on the body
+        isDarkMode = !isDarkMode;
+        body.classList.toggle("custom-dark-mode", isDarkMode);
+        body.classList.toggle("custom-light-mode", !isDarkMode);
+        darkModeToggle.textContent = isDarkMode ? "Light Mode" : "Dark Mode";
     });
 });
